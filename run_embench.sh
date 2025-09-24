eff161c0630c6fb70a0201942b365da2b27f4af3
haraldschn <20490205+haraldschn@users.noreply.github.com>
2025-09-24 08:40:30 +0200

    simple/greedy OoO ALU execute scheduler (external module) + fetch aligner

--- run_embench.sh
+++ run_embench.sh
@@ -5,6 +5,14 @@ TARGET=rv32_4issue
 #TARGET=cv32e40p
 #TARGET=cva6
 
+rm -rf temp
+rm -rf asm_trace
+rm -rf timing_trace
+
+mkdir temp
+mkdir asm_trace
+mkdir timing_trace
+
 PROGRAM=./scripts/run.sh
 directory=target_sw/examples/$TARGET/embench/*
 for file in $directory; do
@@ -12,5 +20,13 @@ for file in $directory; do
     base=$(basename "$file")
 
     echo "Running $PROGRAM with $base"
-    $PROGRAM em:"$base" "$TARGET" > "temp/${base}.log" 2>&1
+    #$PROGRAM em:"$base" "$TARGET" > "temp/${base}.log" 2>&1
+    $PROGRAM em:"$base" "$TARGET" "-ta=asm_trace" "-tp=timing_trace" > "temp/${base}.log" 2>&1
+    mkdir temp/${base}
+    mv asm_trace temp/${base}/asm_trace
+    rm timing_trace/*trace*
+    mv timing_trace temp/${base}/timing_trace
+    mkdir asm_trace
+    mkdir timing_trace
+    #read -p "Press enter to continue"
 done
