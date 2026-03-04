830f80e4f6258aa0b6a7c106097f73e86966dbc2
haraldschn <20490205+haraldschn@users.noreply.github.com>
2026-03-04 12:11:11 +0100

    ListScheduling - CVA6 prototype

--- run_embench.sh
+++ run_embench.sh
@@ -2,8 +2,8 @@
 
 # Iterate over all files in the test_programs directory
 #TARGET=cv32e40p
-#TARGET=cva6
-TARGET=rv32_ooo
+TARGET=cva6
+#TARGET=rv32_ooo
 
 rm -rf temp
 rm -rf asm_trace
@@ -20,13 +20,13 @@ for file in $directory; do
     base=$(basename "$file")
 
     echo "Running $PROGRAM with $base"
-    $PROGRAM em:"$base" "$TARGET" > "temp/${base}.log" 2>&1
-    # $PROGRAM em:"$base" "$TARGET" "-ta=asm_trace" "-tp=timing_trace" > "temp/${base}.log" 2>&1
-    # mkdir temp/${base}
-    # mv asm_trace temp/${base}/asm_trace
-    # rm timing_trace/*trace*
-    # mv timing_trace temp/${base}/timing_trace
-    # mkdir asm_trace
-    # mkdir timing_trace
+    #$PROGRAM em:"$base" "$TARGET" > "temp/${base}.log" 2>&1
+    $PROGRAM em:"$base" "$TARGET" "-ta=asm_trace" "-tp=timing_trace" > "temp/${base}.log" 2>&1
+    mkdir temp/${base}
+    mv asm_trace temp/${base}/asm_trace
+    rm timing_trace/*trace*
+    mv timing_trace temp/${base}/timing_trace
+    mkdir asm_trace
+    mkdir timing_trace
     # read -p "Press enter to continue"
 done
